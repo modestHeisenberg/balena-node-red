449d09f8dabd16cb33c70cc5ae7716261ad71b05
Sam G <sam.isgl@gmail.com>
2021-06-24 09:12:09 -0400

    Patch to get proper time zone and clock time at startup. Added also port 1880 to allow Node-Red browser flow editor

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -4,5 +4,12 @@
 mkdir -p /data/node-red/user/lib/flows || true
 cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 
+# default to UTC if no TIMEZONE env variable is set
+echo "Setting time zone to ${TIMEZONE=UTC}"
+#  this only works for debian based images
+ln -snf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+echo "${TIMEZONE}" > /etc/timezone
+dpkg-reconfigure tzdata
+
 # Start app
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket node-red --settings /usr/src/app/settings.js
